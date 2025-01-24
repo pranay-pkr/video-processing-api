06de6f5cba5f7b0769b26940d9524d4eefeb17a4
pranayreddy604 <pranayreddy579@gmail.com>
2025-01-24 19:07:25 +0530

    Handled errors

--- src/service/videoService.ts
+++ src/service/videoService.ts
@@ -11,37 +11,26 @@ interface VideoMetadata {
 }
 
 class VideoService {
-  // Helper function to get video duration
-  private static getVideoDuration(filePath: string): Promise<number> {
-    return new Promise((resolve, reject) => {
-      ffmpeg.ffprobe(filePath, (err, metadata) => {
-        if (err) {
-          reject(err);
-        }
-        let duration = metadata?.format?.duration || 0;
-        if (!duration) {
-          reject(new Error("Invalid video file"));
-          return;
-        }
-        resolve(duration);
-      });
-    });
-  }
-
   // Video upload service
   static async uploadVideo(file: Express.Multer.File): Promise<VideoMetadata> {
     try {
-      if (!file) throw new Error("No video file uploaded");
+      if (!file) throw new ErrorHandler("No video file uploaded", 400);
 
       // Check file size (max: 25MB)
       if (file.size > 25 * 1024 * 1024) {
-        throw new Error("File size exceeds the maximum limit of 25MB");
+        throw new ErrorHandler(
+          "File size exceeds the maximum limit of 25MB",
+          400
+        );
       }
 
       const duration = await VideoService.getVideoDuration(file.path);
       // Check video duration (min: 5 seconds, max: 25 seconds)
       if (duration < 5 || duration > 25) {
-        throw new Error("Video duration must be between 5 and 25 seconds");
+        throw new ErrorHandler(
+          "Video duration must be between 5 and 25 seconds",
+          400
+        );
       }
 
       const video = await Video.create({
@@ -57,7 +46,10 @@ class VideoService {
     } catch (error: any) {
       console.log(error.message);
       fs.unlinkSync(file.path);
-      throw new Error(error.message);
+      if (error instanceof ErrorHandler) {
+        throw error;
+      }
+      throw new ErrorHandler(error.message, 500);
     }
   }
 
@@ -68,10 +60,10 @@ class VideoService {
   ): Promise<VideoMetadata> {
     try {
       const video = await Video.findByPk(id);
-      if (!video) throw new Error("Video not found");
+      if (!video) throw new ErrorHandler("Video not found", 404);
 
       if (start < 0 || end < 0 || start >= end || end > video.duration) {
-        throw new Error("Invalid start or end time");
+        throw new ErrorHandler("Invalid start or end time", 400);
       }
       const fileName = `trimmed_${crypto.randomUUID()}.mp4`;
       console.log(fileName);
@@ -91,7 +83,10 @@ class VideoService {
       };
     } catch (error: any) {
       console.log(error.message);
-      throw new Error(error.message);
+      if (error instanceof ErrorHandler) {
+        throw error;
+      }
+      throw new ErrorHandler(error.message, 500);
     }
   }
 
@@ -102,7 +97,7 @@ class VideoService {
       });
 
       if (videos.length < 2) {
-        throw new Error("One or more videos not found");
+        throw new ErrorHandler("One or more videos not found", 400);
       }
       const fileName = `merged_video_${crypto.randomUUID()}.mp4`;
       const output = path.join(__dirname, "../..", "uploads", fileName);
@@ -121,15 +116,17 @@ class VideoService {
         id: video.id,
       };
     } catch (error: any) {
-      console.log(error.message);
-      throw new Error(error.message);
+      if (error instanceof ErrorHandler) {
+        throw error;
+      }
+      throw new ErrorHandler(error.message, 500);
     }
   }
 
   static async getSignedUrl(id: string): Promise<string> {
     try {
       const video = await Video.findByPk(id);
-      if (!video) throw new Error("Video not found");
+      if (!video) throw new ErrorHandler("Video not found", 404);
       const payload = {
         id: video.id,
       };
@@ -141,7 +138,10 @@ class VideoService {
       return signedUrl;
     } catch (error: any) {
       console.log(error.message);
-      throw new Error(error.message);
+      if (error instanceof ErrorHandler) {
+        throw error;
+      }
+      throw new ErrorHandler(error.message, 500);
     }
   }
 
@@ -162,7 +162,10 @@ class VideoService {
       return video.path;
     } catch (error: any) {
       console.log(error.message);
-      throw new Error(error.message);
+      if (error instanceof ErrorHandler) {
+        throw error;
+      }
+      throw new ErrorHandler(error.message, 500);
     }
   }
 
@@ -205,6 +208,22 @@ class VideoService {
   private static generateSignedUrl(payload: any) {
     return jwt.sign(payload, constants.JWT_SECRET, {
       expiresIn: "1hr",
+    });
+  }
+  // Helper function to get video duration
+  private static getVideoDuration(filePath: string): Promise<number> {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(filePath, (err, metadata) => {
+        if (err) {
+          reject(err);
+        }
+        let duration = metadata?.format?.duration || 0;
+        if (!duration) {
+          reject(new ErrorHandler("Invalid video file", 400));
+          return;
+        }
+        resolve(duration);
+      });
     });
   }
 }
