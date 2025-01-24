f5f10001351189b3243e2758677ec5a1b75fe554
pranayreddy604 <pranayreddy579@gmail.com>
2025-01-24 13:43:14 +0530

    Expose /upload endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import "dotenv/config";
+import multer from "multer";
+import VideoService from "./service/videoService";
+import VideoController from "./controller/videoController";
+import Video from "./models/video";
 
 const app = express();
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+const upload = multer({ dest: "./uploads/" });
+
+app.post("/upload", upload.single("video"), (req, res) => {
+  VideoController.uploadVideo(req, res);
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  Video.sync();
 });
