8696a973887c502c2c42a28f011093b5d9bb2483
pranayreddy604 <pranayreddy579@gmail.com>
2025-01-24 17:33:27 +0530

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const constants = {
+  PORT: process.env.PORT || "3000",
+  JWT_SECRET: process.env.JWT_SECRET || "",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "1hr",
+  HOST: process.env.HOST || "http://localhost",
+};
