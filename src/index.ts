a995f867d72026ee883f1b44f911a3c56dbfd9cd
pranayreddy604 <pranayreddy579@gmail.com>
2025-01-27 08:46:44 +0530

    Fixes

--- src/index.ts
+++ src/index.ts
@@ -15,34 +15,43 @@ app.use((req, res, next) => {
   authMiddleware(req, res, next);
 });
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../", "uploads")); // Specify the destination folder
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads"));
   },
-  filename: function (req, file, cb) {
-    const fileExtension = file.originalname.split(".").pop(); // Get the file extension
-    const uniqueFilename = `${crypto.randomUUID()}.${fileExtension}`;
-    cb(null, uniqueFilename);
+  filename: (req, file, cb) => {
+    const extension = path.extname(file.originalname); // Get the file extension
+    const uniqueName = `${crypto.randomUUID()}${extension}`;
+    cb(null, uniqueName);
   },
 });
 
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    if (!file.originalname.match(/\.(mp4|avi)$/)) {
-      return cb(new Error(`Please upload a video file of format mp4 or avi`));
+    const allowedExtensions = [".mp4", ".avi"];
+    const fileExtension = path.extname(file.originalname);
+    if (!allowedExtensions.includes(fileExtension)) {
+      return cb(
+        new Error(
+          `Please upload a video file of format ${allowedExtensions.join(
+            " or "
+          )}`
+        )
+      );
     }
     cb(null, true);
   },
 });
-app.post("/api/upload", upload.single("video"), (req, res) => {
+
+app.post("/api/videos/upload", upload.single("video"), (req, res) => {
   VideoController.uploadVideo(req, res);
 });
 
-app.post("/api/merge", (req, res) => {
+app.post("/api/videos/merge", (req, res) => {
   VideoController.mergeVideos(req, res);
 });
 
-app.post("/api/trim", (req, res) => {
+app.post("/api/videos/trim", (req, res) => {
   VideoController.trimVideo(req, res);
 });
 
